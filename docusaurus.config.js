9136ead027917acd36ec2b3256b9d2edb1978373
IannisSimirad <a85268@ualg.pt>
2023-12-21 15:10:10 +0000

    alterar o icon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -68,7 +68,7 @@ const config = {
         title: 'Oficina',
         logo: {
           alt: 'logo',
-          src: 'Logo.ico',
+          src: 'docusaurus.png',
         },
         items: [
           {
