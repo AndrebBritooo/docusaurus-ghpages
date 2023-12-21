3e2ed31afe3d024b3a8b6a347abd669c0a64d0bf
IannisSimirad <a85268@ualg.pt>
2023-12-21 15:52:19 +0000

    alterar logo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Oficina',
   tagline: 'Oficina',
-  favicon: 'Logo.ico',
+  favicon: 'Logo.png',
 
   // Set the production url of your site here
   url: 'https://github.com',
